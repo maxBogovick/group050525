2df06630b976785c0fdf7f83c774ecb1a46dccdc
StanislavPB <perminovsb@gmail.com>
2025-05-09 09:52:53 +0100

    lesson 4

---
+++ src/main/java/lesson04/code/ArithmeticOperations2.java
@@ -0,0 +1,20 @@
+package lesson04.code;
+
+public class ArithmeticOperations2 {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+
+        double result = (double) x / y;
+
+        System.out.println(result);
+
+        System.out.println( 10.0 / 3);
+    }
+}
