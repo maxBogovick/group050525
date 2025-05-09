52d414b52489b8b5e9fca9ddec40feebd60d5f9b
StanislavPB <perminovsb@gmail.com>
2025-05-09 08:49:16 +0100

    add LessonCode01

---
+++ src/main/java/lesson04/LessonCode01.java
@@ -0,0 +1,7 @@
+package lesson04;
+
+public class LessonCode01 {
+    public static void main(String[] args) {
+        int x = 5;
+    }
+}
--- src/main/java/lesson04/MyFirstProgram.java
+++ src/main/java/lesson04/MyFirstProgram.java
@@ -3,5 +3,6 @@ package lesson04;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
     }
 }
