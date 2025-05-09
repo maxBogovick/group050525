a56fa80d60f5578744594fe8417889e46b300bae
StanislavPB <perminovsb@gmail.com>
2025-05-09 10:23:56 +0100

    lesson 4

---
+++ src/main/java/lesson04/code/ArithmeticOperations3.java
@@ -0,0 +1,15 @@
+package lesson04.code;
+
+public class ArithmeticOperations3 {
+
+    public static void main(String[] args) {
+
+        short x = 10;
+        int y = 2_000_000_000;
+
+        long result = (long) x * y;
+
+        System.out.println(result);
+
+    }
+}
---
+++ src/main/java/lesson04/code/CastVariable.java
@@ -0,0 +1,28 @@
+package lesson04.code;
+
+public class CastVariable {
+    public static void main(String[] args) {
+
+        // автоматическое (неявное) приведение типов
+
+        byte xByte = 10;
+
+        short xShort = xByte;
+
+        System.out.println(xShort);
+
+        int xInt = xByte;
+
+        System.out.println(xInt);
+
+        // явное приведение типов
+
+
+        int yInt = 257; // 0000 0001 0000 0001
+
+        byte yByte = (byte) yInt;
+
+        System.out.println(yByte);
+
+    }
+}
