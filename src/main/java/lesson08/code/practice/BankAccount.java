6397eb464a23f9c8b3ca4daec33ca26a1d0c3649
StanislavPB <perminovsb@gmail.com>
2025-05-15 09:09:41 +0100

    lesson 8

---
+++ src/main/java/lesson08/code/practice/BankAccount.java
@@ -0,0 +1,40 @@
+package lesson08.code.practice;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolder;
+    private double accountBalance;
+    private double interestRate;
+
+    public BankAccount(String accountNumber, String accountHolder, double accountBalance, double interestRate) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.accountBalance = accountBalance;
+        this.interestRate = interestRate;
+    }
+
+    public double calculateInterest(int months){
+        double calculatedInterest = accountBalance * interestRate / 100 / 12 * months;
+        return calculatedInterest;
+    }
+
+    public void printAccountInfo(){
+        System.out.println("---------------------------------------------");
+        System.out.println("Текущее состояние счета:");
+        System.out.println("Номер счета: " + accountNumber);
+        System.out.println("Владелец счета: " + accountHolder);
+        System.out.println("Текущий остаток на счете: " + accountBalance);
+        System.out.println("Ставка по депозиту: " + interestRate);
+
+    }
+
+    public void deposit(int amount){
+        accountBalance = accountBalance + amount;
+    }
+
+    public void withdraw(int amount){
+        accountBalance = accountBalance - amount;
+    }
+
+}
---
+++ src/main/java/lesson08/code/practice/BankAccountDemo.java
@@ -0,0 +1,24 @@
+package lesson08.code.practice;
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount("123-456-789","Ruslan",1215,2.5);
+
+        account.printAccountInfo();
+
+        double interest = account.calculateInterest(7);
+
+        System.out.println("За 7 месяцев мы получим доход: " + interest);
+
+        account.deposit(25000);
+
+        interest = account.calculateInterest(7);
+
+        System.out.println("За 7 месяцев мы получим доход: " + interest);
+
+        account.withdraw(12000);
+
+        account.printAccountInfo();
+    }
+}
