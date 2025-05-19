1227ab7ad3cae10da67efb51b5c5e577fa92872f
StanislavPB <perminovsb@gmail.com>
2025-05-19 11:42:52 +0100

    lesson 9

---
+++ src/main/java/lesson09/code/practice/Car.java
@@ -0,0 +1,20 @@
+package lesson09.code.practice;
+
+class Car {
+    private String model;
+    private Manufacturer manufacturer;
+    private String color;
+
+    public Car(String model, Manufacturer manufacturer, String color) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.color = color;
+    }
+
+    public void printCarInfo() {
+        System.out.println("Модель: " + model);
+        System.out.println("Производитель: " + manufacturer.getName() + " (" + manufacturer.getCountry() + ")");
+        System.out.println("Цвет: " + color);
+        System.out.println();
+    }
+}
---
+++ src/main/java/lesson09/code/practice/Category.java
@@ -0,0 +1,19 @@
+package lesson09.code.practice;
+
+class Category {
+    private String name;
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
