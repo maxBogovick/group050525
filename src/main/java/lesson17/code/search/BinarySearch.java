e50665f41b97e53e60de6ee6b0635fc11ae25c91
StanislavPB <perminovsb@gmail.com>
2025-06-03 10:14:56 +0100

    lesson 17

--- src/main/java/lesson17/code/search/BinarySearch.java
+++ src/main/java/lesson17/code/search/BinarySearch.java
@@ -1,5 +1,7 @@
 package lesson17.code.search;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
 
@@ -96,5 +98,23 @@ public class BinarySearch {
 Сложность алгоритма поиска - логарифмическая - O(log n)
 
          */
+
+        int[] arrayForSearch = {1,3,5,7,9,12,15,19,21,26,29,35,39,43,48,50,100};
+
+        int elementForSearch = 19;
+
+        SearchInArray search = new SearchInArray();
+
+        int indexSearch = search.binarySearch(arrayForSearch, elementForSearch);
+
+        if (indexSearch >= 0 ) {
+            System.out.println("Число: " + elementForSearch + " найдено в массиве и имеет индекс: " + indexSearch);
+        } else {
+            System.out.println("Число: " + elementForSearch + " в массиве не найдено");
+        }
+
+        // найдем индекс для числа 29
+
+        System.out.println("Индекс числа 29 -> " + Arrays.binarySearch(arrayForSearch, 29));
     }
 }
--- src/main/java/lesson17/code/search/SearchInArray.java
+++ src/main/java/lesson17/code/search/SearchInArray.java
@@ -11,4 +11,28 @@ public class SearchInArray {
 
         return -1;
     }
+
+    public int binarySearch(int[] workingArray, int searchElement) {
+        int leftIndex = 0;
+        int rightIndex = workingArray.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            // если середина - это наш искомый элемент
+            if (workingArray[middleIndex] == searchElement) {
+                return middleIndex;
+            }
+
+            // проверяем какую (левую или правую) части массива выбрать для продолжения поиска
+
+            if (workingArray[middleIndex] < searchElement) {
+                leftIndex = middleIndex + 1;
+            } else {
+                rightIndex = middleIndex - 1;
+            }
+        }
+        return -1;
+
+    }
 }
