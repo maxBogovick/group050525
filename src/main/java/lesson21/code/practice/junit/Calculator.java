eaaa8c62f1409454c04bb088fb2a261160287738
StanislavPB <perminovsb@gmail.com>
2025-06-11 10:23:44 +0100

    lesson 21

---
+++ src/main/java/lesson21/code/practice/junit/Calculator.java
@@ -0,0 +1,12 @@
+package lesson21.code.practice.junit;
+
+public class Calculator {
+
+    public int sum(int x, int y){
+        return x + y;
+    }
+
+    public int multiplication(int x, int y){
+        return x * y;
+    }
+}
